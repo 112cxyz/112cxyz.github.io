b861f83050b45cae9a56b9efdb1e435cb90ee6fb
Toby Fox <tobyfox2020@gmail.com>
2024-09-12 03:07:23 +0100

    Delete installer.sh

--- installer.sh
+++
@@ -1,8 +0,0 @@
-clear
-echo Goofy ah bypass
-echo Installing Files...
-sleep 6
-echo '<title>Google</title><link rel="icon" type="image/x-icon" href="google.ico"><iframe src="https://tree.tsandcs.tk" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">ifram</iframe>' >> ~/Downloads/Files.html
-echo File1 Installed
-sleep 2
-echo Check The Downloads Folder 
